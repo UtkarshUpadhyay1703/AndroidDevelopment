5f6001e5195300744f0e20bab34b60f22a0d436c
UTKARSH UPADHYAY <utkarsh1703@gmail.com>
2023-10-08 00:07:06 +0530

    Done with first Battery display

---
+++ Battery Display/app/src/main/java/com/example/displaybatterydetails/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.displaybatterydetails
+
+import android.os.BatteryManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Declare button, that will show battery percentage when clicked
+            val btn = findViewById<Button>(R.id.showBtn)
+
+        btn.setOnClickListener{
+
+            // Call battery manager service
+            val bm = applicationContext.getSystemService(BATTERY_SERVICE) as BatteryManager
+
+            // Get the battery percentage and store it in a INT variable
+            val batLevel:Int = bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+
+            // Display the variable using a Toast
+            Toast.makeText(applicationContext,"Battery is $batLevel%",Toast.LENGTH_LONG).show()
+        }
+    }
+}
